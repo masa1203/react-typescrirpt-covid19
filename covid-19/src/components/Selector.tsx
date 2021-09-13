281fdd3a69f54b744221e36c4b946e25bc134441
masa1203 <msys.sawada@gmail.com>
2021-09-13 23:45:09 +0900

    getCountryDataをつかって国別のデータを取得するようにした

--- covid-19/src/components/Selector.tsx
+++ covid-19/src/components/Selector.tsx
@@ -9,10 +9,15 @@ interface InterfaceCountry {
 const Selector = () => {
   const [country, setCountry] = useState<string>("");
   // 選択したcountryのデータを取得する
-  const getCountryData = () => {};
+  const getCountryData = () => {
+    fetch(`https://api.covid19api.com/country/${country}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div>
       <p>セレクタ-</p>
+      <option>Select A Country</option>
       {/* e: イベントパラメータ */}
       <select
         onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
@@ -26,6 +31,7 @@ const Selector = () => {
         ))}
       </select>
       {country}
+      <button onClick={getCountryData}>Get Data</button>
     </div>
   );
 };
