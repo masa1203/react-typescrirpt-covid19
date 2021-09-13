c499aed64ac9e252dd8a4178581c6acdc0df5fa5
masa1203 <msys.sawada@gmail.com>
2021-09-13 23:10:17 +0900

    countryJsonをJSX内に移動してみてみる

--- covid-19/src/components/Selector.tsx
+++ covid-19/src/components/Selector.tsx
@@ -1,12 +1,21 @@
 import countriesJson from "../countries.json";
 
+interface TypeCountry {
+  Country: string;
+  Slug: string;
+}
+
 const Selector = () => {
   console.log(countriesJson);
-  countriesJson.map((country) => console.log(country));
+  //   countriesJson.map((country) => console.log(country));
   return (
     <div>
       <p>セレクタ-</p>
       <select>
+        {countriesJson.map((country: TypeCountry) => {
+          console.log(country.Country);
+          console.log(country.Slug);
+        })}
         <option>United Kingdom</option>
         <option>China</option>
         <option>Japan</option>
