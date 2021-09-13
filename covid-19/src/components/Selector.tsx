2dc7411f46b1a62163c9f373e518843ff1f83661
masa1203 <msys.sawada@gmail.com>
2021-09-13 22:36:32 +0900

    セレクタにサンプルを入れてみる

--- covid-19/src/components/Selector.tsx
+++ covid-19/src/components/Selector.tsx
@@ -2,6 +2,11 @@ const Selector = () => {
   return (
     <div>
       <p>セレクタ-</p>
+      <select>
+        <option>United Kingdom</option>
+        <option>China</option>
+        <option>Japan</option>
+      </select>
     </div>
   );
 };
