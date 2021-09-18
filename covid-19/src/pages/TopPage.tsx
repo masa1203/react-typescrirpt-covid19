5abdbb6f5c1b87612b8145e7310a4458bd7d86c0
masa1203 <msys.sawada@gmail.com>
2021-09-18 14:18:39 +0900

    Results.tsxを追加してTopPageで表示させた

--- covid-19/src/pages/TopPage.tsx
+++ covid-19/src/pages/TopPage.tsx
@@ -1,11 +1,13 @@
 import Title from "../components/Title";
 import Selector from "../components/Selector";
+import Results from "../components/Results";
 
 const TopPage = () => {
   return (
     <div>
       <Title />
       <Selector />
+      <Results />
     </div>
   );
 };
