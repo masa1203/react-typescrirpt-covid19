514c3d838d19bc8cfa8aff48e6d265ce659aa04f
masa1203 <msys.sawada@gmail.com>
2021-09-18 18:49:12 +0900

    Resultの表示を変更

--- covid-19/src/components/Results.tsx
+++ covid-19/src/components/Results.tsx
@@ -9,13 +9,14 @@ type TypeResults = {
 };
 
 const Results = (props: TypeResults) => {
+  console.log("Results.props:", props);
   return (
     <div>
-      <p>日付:{props.countryData.date}</p>
-      {/* <p>新規感染者:{props.countryData.newConfirmed}</p> */}
-      <p>感染者総数:{props.countryData.totalConfirmed}</p>
-      {/* <p>新規回復者:{props.countryData.newRecovered}</p> */}
-      <p>回復者総数:{props.countryData.totalRecoverd}</p>
+      <p>日付:{props.countryData.date.slice(0, 10)}</p>
+      {/* <p>新規感染者:{props.countryData.newConfirmed.toLocaleString()}</p> */}
+      <p>感染者総数:{props.countryData.totalConfirmed.toLocaleString()}</p>
+      {/* <p>新規回復者:{props.countryData.newRecovered.toLocaleString()}</p> */}
+      <p>回復者総数:{props.countryData.totalRecoverd.toLocaleString()}</p>
     </div>
   );
 };
