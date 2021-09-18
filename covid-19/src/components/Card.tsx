07209a9026aa879950004f46dc7369cd7a1d75fc
masa1203 <msys.sawada@gmail.com>
2021-09-18 21:31:33 +0900

    サマリデータをfetchして確認するところまでできた

--- covid-19/src/components/Card.tsx
+++ covid-19/src/components/Card.tsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
+interface InterfaceAllCountriesData {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: {};
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
 const Card = () => {
+  const [allCountriesData, setAllCountriesData] = useState<
+    InterfaceAllCountriesData[]
+  >([
+    {
+      Country: "",
+      CountryCode: "",
+      Date: "",
+      ID: "",
+      NewConfirmed: 0,
+      NewDeaths: 0,
+      NewRecovered: 0,
+      Premium: {},
+      Slug: "",
+      TotalConfirmed: 0,
+      TotalDeaths: 0,
+      TotalRecovered: 0,
+    },
+  ]);
+  const getAllCountriesData = () => {
+    fetch("https://api.covid19api.com/summary")
+      .then((res) => res.json())
+      .then((data) => setAllCountriesData(data.Countries));
+  };
   return (
     <div>
+      {console.log(allCountriesData)}
       <h1>ワールド</h1>
+      <button onClick={() => getAllCountriesData()}>
+        Get All Countries Data
+      </button>
+      {allCountriesData.map((singleCountryData) =>
+        console.log(singleCountryData)
+      )}
     </div>
   );
 };
