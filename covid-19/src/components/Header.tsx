10fe7892472a934e17cf4130eb346d5955e49f0a
masa1203 <msys.sawada@gmail.com>
2021-09-18 22:13:35 +0900

    HeaderコンポーネントにLinkタグを追加して分岐できるようにした

--- covid-19/src/components/Header.tsx
+++ covid-19/src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <Link to="/">国ごとの感染状況</Link>
+      <Link to="/world">世界の感染状況</Link>
     </div>
   );
 };
--- covid-19/src/pages/WorldPage.tsx
+++ covid-19/src/pages/WorldPage.tsx
@@ -1,9 +1,11 @@
+import Header from "../components/Header";
 import Card from "../components/Card";
 import Title from "../components/Title";
 
 const WorldPage = (props: any) => {
   return (
     <div>
+      <Header />
       <Title />
       <Card allCountriesData={props.allCountriesData} />
     </div>
