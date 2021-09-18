51f4a50b19962f48705b07efe157f228fa0ede52
masa1203 <msys.sawada@gmail.com>
2021-09-18 18:59:24 +0900

    型をInterfaceに統一して名前を変更した

--- covid-19/src/App.tsx
+++ covid-19/src/App.tsx
@@ -1,39 +1,42 @@
-import React from "react";
-import countriesJson from "./countries.json";
 import { useState } from "react";
+import countriesJson from "./countries.json";
 import TopPage from "./pages/TopPage";
 import "./App.css";
 
-type setCountryDataType = {
+interface InterfaceSetCountryData {
   date: string;
-  // newConfirmed: string;
+  newConfirmed: string;
   totalConfirmed: string;
-  // newRecovered: string;
+  newRecovered: string;
   totalRecoverd: string;
-};
+}
 
 function App() {
   const [country, setCountry] = useState<string>("");
-  const [countryData, setCountryData] = useState<setCountryDataType>({
+  const [countryData, setCountryData] = useState<InterfaceSetCountryData>({
     date: "",
-    // newConfirmed: "",
+    newConfirmed: "",
     totalConfirmed: "",
-    // newRecovered: "",
+    newRecovered: "",
     totalRecoverd: "",
   });
 
   // 選択したcountryのデータを取得する
   const getCountryData = () => {
-    console.log("getCountryData");
+    console.log("getCountryData start!");
     fetch(`https://api.covid19api.com/country/${country}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setCountryData({
           date: data[data.length - 1].Date,
-          // newConfirmed: data[data.length - 2].Confirmed,
+          newConfirmed: String(
+            data[data.length - 1].Confirmed - data[data.length - 2].Confirmed
+          ),
           totalConfirmed: data[data.length - 1].Confirmed,
-          // newRecovered: data[data.length - 2].Recovered,
+          newRecovered: String(
+            data[data.length - 1].Recovered - data[data.length - 2].Recovered
+          ),
           totalRecoverd: data[data.length - 1].Recovered,
         });
       });
@@ -41,7 +44,7 @@ function App() {
 
   return (
     <div>
-      {console.log("countryData", countryData)}
+      {console.log("App.countryData", countryData)}
       <TopPage
         countriesJson={countriesJson}
         setCountry={setCountry}
--- covid-19/src/components/Results.tsx
+++ covid-19/src/components/Results.tsx
@@ -1,21 +1,21 @@
-type TypeResults = {
+interface InterfaceResults {
   countryData: {
     date: string;
-    // newConfirmed: string;
+    newConfirmed: string;
     totalConfirmed: string;
-    // newRecovered: string;
+    newRecovered: string;
     totalRecoverd: string;
   };
-};
+}
 
-const Results = (props: TypeResults) => {
+const Results = (props: InterfaceResults) => {
   console.log("Results.props:", props);
   return (
     <div>
       <p>日付:{props.countryData.date.slice(0, 10)}</p>
-      {/* <p>新規感染者:{props.countryData.newConfirmed.toLocaleString()}</p> */}
+      <p>新規感染者:{props.countryData.newConfirmed.toLocaleString()}</p>
       <p>感染者総数:{props.countryData.totalConfirmed.toLocaleString()}</p>
-      {/* <p>新規回復者:{props.countryData.newRecovered.toLocaleString()}</p> */}
+      <p>新規回復者:{props.countryData.newRecovered.toLocaleString()}</p>
       <p>回復者総数:{props.countryData.totalRecoverd.toLocaleString()}</p>
     </div>
   );
